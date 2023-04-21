73e57160b16c6d5be0dafb0d61dee288db8d0134
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-21 16:40:24 +0100

    partial commit working on blog detail blog summary work

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useState } from "react";
+
+const BlogDetail = ({ blog, style, handleToggle }) => {
+  const { title, author, url, likes } = blog;
+  return (
+    <div style={style}>
+      <p>
+        {title} <button onClick={handleToggle}>hide</button>
+      </p>
+      <p>{url}</p>
+      <p>Likes: {likes}</p>
+      <p>{author}</p>
+    </div>
+  );
+};
+
+const BlogSummary = ({ blog, style }) => (
+  <div style={style}>
     {blog.title} {blog.author}
   </div>
 );
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [showDetail, setShowDetail] = useState(false);
+  const handleToggle = () => setShowDetail(!showDetail);
+  // return <BlogDetail style={blogStyle} blog={blog} />;
+  return <BlogSummary blog={blog} style={blogStyle} />;
+};
 
 export default Blog;
