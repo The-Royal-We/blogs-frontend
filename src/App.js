0a3dc0e394f029589cafa2e5b5ce0a92b9a12fd2
Brendan Carroll <brendan.carroll@workhuman.com>
2023-05-05 16:07:06 +0100

    complete 5.10

--- src/App.js
+++ src/App.js
@@ -164,13 +164,15 @@ const App = () => {
           </div>
           {createBlogForm()}
           <div>
-            {blogs.map((blog) => (
-              <Blog
-                key={blog.id}
-                blog={blog}
-                handleLikeButton={handleLikeButton}
-              />
-            ))}
+            {blogs
+              .sort((a, b) => b.likes - a.likes)
+              .map((blog) => (
+                <Blog
+                  key={blog.id}
+                  blog={blog}
+                  handleLikeButton={handleLikeButton}
+                />
+              ))}
           </div>
         </div>
       )}
